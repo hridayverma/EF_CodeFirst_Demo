9d05c63cbab312432e9034de39e3005d689be04e
hridayverma <hrid.06@gmail.com>
2022-08-17 13:32:21 +0530

    Add project files.

---
+++ EF_CodeFirst_Demo/Controllers/StudnetController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EF_CodeFirst_Demo.Models;//to access DBContext and Model classes
+
+namespace EF_CodeFirst_Demo.Controllers
+{
+    public class StudnetController : Controller
+    {
+        // GET: Studnet
+        PGDACContext db = new PGDACContext();
+        public ActionResult Index()
+        {
+            //ViewBag.DbName = db.Database.Connection.Database;
+            //List<Student> data=db.Students.ToList();
+            return View(db.Students.Include("Address").ToList());
+        }
+
+        //for insertion
+        public ViewResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                //insert work
+                db.Students.Add(student);//add will save passed entity in current context
+                db.SaveChanges();//will update the changes in database
+                return RedirectToAction("Index");//redirecting to Index Action
+            }
+            return View(student);
+        }
+
+
+
+
+
+    }
+}
---
+++ EF_CodeFirst_Demo/Models/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EF_CodeFirst_Demo.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }  //scalar property
+        public string StudentName { get; set; }
+        public string DateOfBirth { get; set; }
+        public Address Address { get; set; }//navigation property
+
+       // public string Email { get; set; }
+
+    }
+    public class Address
+    {
+        public int Id { get; set; }
+        public string AddressLine { get; set; }
+        public string City { get; set; }
+        public ICollection<Student> Students { get; set; }
+    }
+
+
+
+
+}
